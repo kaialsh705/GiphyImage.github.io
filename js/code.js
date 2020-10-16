92edd45d1e72523f2c41caf43b29540ac892ed8f
kailash <59761075+kaialsh705@users.noreply.github.com>
2020-10-16 11:36:39 -0700

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,7 +1,6 @@
 window.addEventListener('load',bindEvent)
 function bindEvent(){
-    document.querySelector("#bt").addEventListener('click',doAjax2);
-    document.querySelector("#record").addEventListener('click',speech)
+    document.querySelector("#bt").addEventListener('click',doAjax);
 }
 function printImage(obj){
     var div=document.querySelector("#result");
@@ -18,7 +17,7 @@ function createImage(url){
     
 
 }
-function doAjax2(){
+function doAjax(){
     var txt=document.querySelector("#txt").value;
     var url=`http://api.giphy.com/v1/gifs/search?api_key=477KU4aQNgo78dawcv7IQkuFsMRUilol&q=${txt}&limit=8`;
     if(window.fetch){
@@ -26,45 +25,7 @@ function doAjax2(){
         promise.then(response=>{
             response.json().then(data=>{
                 printImage(data);
-            }).catch(err=>{
-                console.log("Invalid Json",err);
-            }).catch(e=>{
-                console.log("server call error",e);
-            }).finaliy(()=>{
-                console.log("server time data fetching error")
             })
         })
-    }else{
-        console.log("fetch not supported in ur browser");
     }
-}       
-// function doAjax(){
-//     var url=`http://api.giphy.com/v1/gifs/search?api_key=477KU4aQNgo78dawcv7IQkuFsMRUilol&q=${txt}&limit=8`;
-    
-//     console.log("Url is",url);
-
-//     var http=new XMLHttpRequest();
-//     //bind readySate init
-//     http.onreadystatechange=function(){
-//         console.log("state is ready",http.readyState,"status is",http.status);
-//         if(http.readyState==4 && http.status==200){
-//             var obj=JSON.parse(http.responseText);
-//             console.log("Object is",obj);
-//             printImage(obj)
-//         }
-//     }
-//    http.open('GET',url);
-//    http.send();
-// }
-// function speech(){
-//     window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
-    
-//     const recognitaion=new SpeechRecognition();
-//     recognitaion.lang="en-GB";
-//     recognitaion.onresult=(event)=>{
-//         console.log("event is working",event);
-//         document.querySelector('speechRecord').Value=event.results[0][0].transcript;
-//     }
-//     recognitaion.start();
-// }
-
+};       
